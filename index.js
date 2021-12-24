d6a6b5089c5e2b24c63e37634c70fe3109c4f507
Musfiqur Rahma <musfiq371@gmail.com>
2021-12-24 12:55:19 +0600

    added get

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ async function run() {
   run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!This is my Royal World')
 })
 
 app.listen(port, () => {
